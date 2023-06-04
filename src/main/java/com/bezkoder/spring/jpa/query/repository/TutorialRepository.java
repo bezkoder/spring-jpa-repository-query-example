1f3d32f70ee1164d609cc392c6887e7dde20111f
tienbku <tien.ngtruong@gmail.com>
2023-06-04 15:11:28 +0700

    update to Spring Boot 3

--- src/main/java/com/bezkoder/spring/jpa/query/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/jpa/query/repository/TutorialRepository.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
